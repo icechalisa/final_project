618619182c003d341a4458ba621526209dd68e2f
Chalisa <Chalisa.ki@ku.th>
2023-11-22 17:21:36 +0700

    create 2 class: database and table

--- database.py
+++ database.py
@@ -12,6 +12,78 @@ with open(os.path.join(__location__, 'persons.csv')) as f:
         persons.append(dict(r))
 print(persons)
 
+
+class Database:
+    def __init__(self):
+        self.database = []
+
+    def insert(self, table):
+        self.database.append(table)
+
+    def search(self, table_name):
+        for table in self.database:
+            if table.table_name == table_name:
+                return table
+        return None
+
+
+import copy
+
+
+class Table:
+    def __init__(self, table_name, table):
+        self.table_name = table_name
+        self.table = table
+
+    def join(self, other_table, common_key):
+        joined_table = Table(
+            self.table_name + '_joins_' + other_table.table_name, [])
+        for item1 in self.table:
+            for item2 in other_table.table:
+                if item1[common_key] == item2[common_key]:
+                    dict1 = copy.deepcopy(item1)
+                    dict2 = copy.deepcopy(item2)
+                    dict1.update(dict2)
+                    joined_table.table.append(dict1)
+        return joined_table
+
+    def filter(self, condition):
+        filtered_table = Table(self.table_name + '_filtered', [])
+        for item1 in self.table:
+            if condition(item1):
+                filtered_table.table.append(item1)
+        return filtered_table
+
+    def aggregate(self, function, aggregation_key):
+        temps = []
+        for item1 in self.table:
+            temps.append(float(item1[aggregation_key]))
+        return function(temps)
+
+    def select(self, attributes_list):
+        temps = []
+        for item1 in self.table:
+            dict_temp = {}
+            for key in item1:
+                if key in attributes_list:
+                    dict_temp[key] = item1[key]
+            temps.append(dict_temp)
+        return temps
+
+    def insert(self, data):
+        self.table.append(data)
+
+    def update(self, column, id, value):
+        filtered_table = Table(self.table_name + '_filtered', [])
+        for item1 in self.table:
+            # condition ID match
+            if item1['ID'] == id:
+                item1[column] = value
+        return filtered_table
+
+    def __str__(self):
+        return self.table_name + ':' + str(self.table)
+
 # add in code for a Database class
 
 # add in code for a Table class
